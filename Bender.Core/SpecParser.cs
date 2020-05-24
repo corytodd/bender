eab05476c96415354dd30432bcb4abe4d38cccc0
Cory Todd <cory@ardonyx.io>
2020-05-24 16:52:22 -0700

    Captured nest yaml errors
    The inner exception of the yaml lib now contain
    the interesting information.

--- Bender.Core/SpecParser.cs
+++ Bender.Core/SpecParser.cs
@@ -1,4 +1,6 @@
-﻿namespace Bender.Core
+﻿using System.Text;
+
+namespace Bender.Core
 {
     using System.Collections.Generic;
     using YamlDotNet.Serialization;
@@ -20,23 +22,29 @@
         {
             try
             {
-                using (var reader = file.AsStringReader())
-                {
-                    var parser = new MergingParser(new Parser(reader));
-                    var deserializer = new DeserializerBuilder()
-                        .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                        .Build();
+                using var reader = file.AsStringReader();
+                var parser = new MergingParser(new Parser(reader));
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                    .Build();
 
-                    return deserializer.Deserialize<SpecFile>(parser);
-                }
+                return deserializer.Deserialize<SpecFile>(parser);
             }
             catch (YamlException ex)
             {
-                throw new ParseException(ex.Message);
+                var sb = new StringBuilder(ex.Message);
+                var inner = ex.InnerException;
+                while(inner != null)
+                {
+                    sb.AppendLine(inner.Message);
+                    inner = inner.InnerException;
+                }
+
+                throw new ParseException(sb.ToString());
             }
             catch (KeyNotFoundException ex)
             {
-                var err = string.Format("Malformed YAML, try running through an online validator: {0}", ex.Message);
+                var err = $"Malformed YAML, try running through an online validator: {ex.Message}";
                 throw new ParseException(err);
             }
         }
